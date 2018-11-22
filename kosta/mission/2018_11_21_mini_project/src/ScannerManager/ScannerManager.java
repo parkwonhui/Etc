068a5e9a267af55c1a443a02d65f4ec0a16aebb6
parkwonhui <parkwonhui.dev@gmail.com>
2018-11-22 23:13:56 +0900

    modify 입력 클래스 Scanner->BufferReader로 변경

--- kosta/mission/2018_11_21_mini_project/src/ScannerManager/ScannerManager.java
+++ kosta/mission/2018_11_21_mini_project/src/ScannerManager/ScannerManager.java
@@ -1,7 +1,31 @@
 package ScannerManager;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class ScannerManager {
-	public static Scanner sc = new Scanner(System.in);
+	static BufferedReader in;
+	static {
+		 in = new BufferedReader(new InputStreamReader(System.in));
+	}
+	 
+	static public String ReadString() throws Exception{
+		String str = null;
+		in = new BufferedReader(new InputStreamReader(System.in));
+		str = in.readLine();
+		return str;
+	}
+	
+	static public int ReadInt() throws Exception {
+		int value = 0;
+		in = new BufferedReader(new InputStreamReader(System.in));
+		String str = in.readLine();
+		value = Integer.parseInt(str);
+	
+		return value;
+	}
+
 }
