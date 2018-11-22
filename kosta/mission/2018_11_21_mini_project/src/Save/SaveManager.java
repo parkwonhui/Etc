0d5013e329de5f8337eb7fe9fa44dda99483ef99
parkwonhui <parkwonhui.dev@gmail.com>
2018-11-22 16:29:03 +0900

    add 장바구니 현재 담은 메뉴 출력, 전체 메뉴 계산(할인, 커피 휘핑크림 옵션 등...) 추가

--- kosta/mission/2018_11_21_mini_project/src/Save/SaveManager.java
+++ kosta/mission/2018_11_21_mini_project/src/Save/SaveManager.java
@@ -3,7 +3,11 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Map;
 
+
+import Menu.Coffee;
+import Menu.Dessert;
 import Menu.Menu;
+import Menu.NonCoffee;
 
 public class SaveManager {
 	// 장바구니
@@ -22,15 +26,104 @@ public class SaveManager {
 		return true;
 	}
 	
-	public void allSavePrint(){
+	public int calSetMenu(){		// 세트 체크 개수를 리턴한다
+		
+		int drinkCount = 0;
+		int dessertCount = 0;
+		for(int i =0; i < saveList.size(); ++i){
+			Menu menu = saveList.get(i);
+			if(menu instanceof Coffee) 			++drinkCount;
+			else if(menu instanceof NonCoffee) 	++drinkCount;
+			else if(menu instanceof Dessert) 	++dessertCount;
+		}
+		
+		if(drinkCount >= dessertCount)
+			return dessertCount;
+		
+		return drinkCount;
+	}
+
+	public void allPrint(){
+		System.out.println("******* 구매리스트 ********");		
+		for(int i =0; i < saveList.size(); ++i){
+			Menu menu = saveList.get(i);
+			System.out.println(i+":"+menu.getName());
+		}
+	}
+	
+	public void calAllMenu(){
 		System.out.println("******* 담은 메뉴 ********");
+		int total = getCoffeeMoney()+getSeasonMoney()+getDessertMoney();
+		System.out.println("**** 총가격 :"+total+"**** ");		
+	}
+	
+	private int getCoffeeMoney(){
 		int allPrice = 0;
 		for(int i =0; i < saveList.size(); ++i){
 			Menu menu = saveList.get(i);
+			if(false == (menu instanceof Coffee))
+				continue;
+			
 			allPrice += menu.getPrice();
+			int optionValue = addOptionMoney((Coffee)menu);
+			allPrice += optionValue;
+			
+			System.out.println("["+(i+1)+"]이름:"+menu.getName()+"옵션추가:"+optionValue+": 가격:"+menu.getPrice());
+		}
+		return allPrice;
+	}
+	
+	private int getSeasonMoney(){
+		int allPrice = 0;
+		for(int i =0; i < saveList.size(); ++i){
+			Menu menu = saveList.get(i);
+			if(false == (menu instanceof NonCoffee))
+				continue;
+			
+			allPrice += menu.getPrice();
+
 			System.out.println("["+(i+1)+"]이름:"+menu.getName()+": 가격:"+menu.getPrice());
 		}
-		System.out.println("**** 총가격 :"+allPrice+"**** ");		
+		return allPrice;
+	}
+	
+	private int getDessertMoney(){
+		int allPrice = 0;
+		int setMenuCount = calSetMenu();
+		int setCount = 0;
+		int resultMoney = 0;			// 결과금액
+		int setDiscount = 0;			// 할인금액
+		for(int i =0; i < saveList.size(); ++i){
+			Menu menu = saveList.get(i);
+			if(false == (menu instanceof Dessert))
+				continue;
+			
+			int price = menu.getPrice();
+			if(setCount < setMenuCount){				// 할인금액 적용
+				Dessert dessert = (Dessert)menu;
+				resultMoney = (int)(price*dessert.getDiscount()*0.01);
+				System.out.println("price:"+price+"resultMoney:"+resultMoney);
+				setDiscount = (int) (price - resultMoney);
+				price += resultMoney;
+				++setCount;
+			}
+			
+			allPrice += price;
+
+			System.out.println("["+(i+1)+"]이름:"+menu.getName()+": 가격:"+menu.getPrice()+"세트할인금액:"+setDiscount);
+		}
+		return allPrice;
+	}
+
+	private int addOptionMoney(Coffee coffee){
+		int optionMoney = 0;
+		final int option = 200;		// option은 200원씩
+		if(true == coffee.isbAddShot())		optionMoney+=option;
+		if(true == coffee.isbSizeup())		optionMoney+=option;
+		if(true == coffee.isbSyrup())		optionMoney+=option;
+		if(true == coffee.isbWhippedCream())optionMoney+=option;
+		
+		return optionMoney;
 	}
 	
 	public int getSize(){
@@ -45,10 +138,6 @@ public class SaveManager {
 	}
 	
 	public void deleteAllMenu(){
-		int count = saveList.size();
-		for(int i = 0; i < count; ++i)
-			saveList.remove(i);
-		
+		saveList.clear();
 	}
-	
 }
